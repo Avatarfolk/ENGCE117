5c3084626ac12ddf6ac3a1b9e5ab9282512484d3
chanon ponjan <141479289+Avatarfolk@users.noreply.github.com>
2023-12-21 00:24:13 +0700

    Add files via upload

--- Lab2/Lab2.3.cpp
+++ Lab2/Lab2.3.cpp
@@ -7,7 +7,7 @@ void explode( char str1[], char splitter, char str2[][10], int *count ) {
     int n , z = 0 ;
     *count = 0 ; 
     
-    for (n = 0 ; str1[n] != 0 ; ++n ) {
+    for (n = 0 ; str1[n] ; ++n ) {
         if (str1[n] != splitter) {
             str2[*count][z++] = str1[n] ;
         } else {
@@ -28,8 +28,8 @@ int main() {
     int num ;
     explode( "I/Love/You", '/', out, &num ) ;
 
-    for (int i = 0; i < num; ++i) {
-        printf( "str2[%d] = \"%s\"\n", i, out[i] ) ;
+    for (int n = 0; n < num ; ++n) {
+        printf( "str2[%d] = \"%s\"\n", n, out[n] ) ;
     }
     printf( "\n" ) ;
     printf( "count : %d\n", num ) ;
