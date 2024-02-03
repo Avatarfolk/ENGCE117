e2cf015e706121413d92deffce4d293d212274b0
chanon ponjan <141479289+Avatarfolk@users.noreply.github.com>
2024-02-03 17:53:38 +0700

    Add files via upload

--- Lab6/Lab6.1.cpp
+++ Lab6/Lab6.1.cpp
@@ -40,45 +40,45 @@ void ShowAll( struct studentNode *walk ) {
 
 struct studentNode *AddNode( struct studentNode **walk, char n[], int a, char s, float g ) {
 
-    struct studentNode *temp = NULL;
+    struct studentNode *temp = NULL ;
 	while( *walk != NULL ){
-		temp = *walk; 
-		walk = &(*walk)->next;
+		temp = *walk ; 
+		walk = &(*walk)->next ;
 	}
-	*walk = new struct studentNode;
-	strcpy((*walk)->name, n);
-	(*walk)->age = a;
-	(*walk)->sex = s;
-	(*walk)->gpa = g;
-	(*walk)->next = NULL;
-	(*walk)->back = temp;
-	return *walk;
+	*walk = new struct studentNode ;
+	strcpy( ( *walk )->name, n ) ;
+	( *walk )->age = a ;
+	( *walk )->sex = s ;
+	( *walk )->gpa = g ;
+	( *walk )->next = NULL ;
+	( *walk )->back = temp ;
+	return *walk ;
 }
-void InsNode(struct studentNode *ins, char n[], int a, char s, float g){
-	ins->back->next = new struct studentNode;
-	strcpy(ins->back->next->name, n);
-	ins->back->next->age = a;
-	ins->back->next->sex = s;
-	ins->back->next->gpa = g;
-	ins->back->next->next = ins;
-	ins->back->next->back = ins->back;
-	ins->back = ins->back->next;
+void InsNode( struct studentNode *ins, char n[], int a, char s, float g ){
+	ins->back->next = new struct studentNode ;
+	strcpy( ins->back->next->name, n ) ;
+	ins->back->next->age = a ;
+	ins->back->next->sex = s ;
+	ins->back->next->gpa = g ;
+	ins->back->next->next = ins ;
+	ins->back->next->back = ins->back ;
+	ins->back = ins->back->next ;
 }
-void GoBack( struct studentNode **back ) {
-  *back = (*back)->back;
+void GoBack(  struct studentNode **back ) {
+  *back = ( *back )->back ;
 } 
 
 void DelNode( struct studentNode *del ){
-	struct studentNode *temp;
-	del->back->next = del->next;
+	struct studentNode *temp ;
+	del->back->next = del->next ;
 	if( del->next != NULL ){
-		del->next->back = del->back;
-		temp = del->next;
+		del->next->back = del->back ;
+		temp = del->next ;
 	}else{
-		temp = del->back;
+		temp = del->back ;
 	}
-	delete del;
-	del = temp;
+	delete del ;
+	del = temp ;
 } 
 
 
